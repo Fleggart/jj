d016084e9ef2c3294210d619b301693130b701e0
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-05 23:05:47 +0800

    Create AncientDebrisSpawner.java

---
+++ src/main/java/Fleggart/worldgen/AncientDebrisSpawner.java
@@ -0,0 +1,29 @@
+package Fleggart.worldgen;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+
+import java.util.Random;
+
+public class AncientDebrisSpawner implements IWorldGenerator {
+
+    private final WorldGenerator generator = new AncientDebrisGenerator();
+    private static final int CHANCE = 300;
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world,
+                         IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+
+        if (world.provider.getDimension() != 0) return;
+        if (random.nextInt(CHANCE) != 0) return;
+
+        int x = chunkX * 16 + 8;
+        int z = chunkZ * 16 + 8;
+        int y = world.getTopSolidOrLiquidBlock(new BlockPos(x, 0, z)).getY();
+
+        generator.generate(world, random, new BlockPos(x, y, z));
+    }
+}

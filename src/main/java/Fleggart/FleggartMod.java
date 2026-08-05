ef94b4aab29071db1184af67733b14760f89632b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-05 23:02:39 +0800

    Create FleggartMod.java

---
+++ src/main/java/Fleggart/FleggartMod.java
@@ -0,0 +1,16 @@
+package Fleggart;
+
+import Fleggart.worldgen.AncientDebrisSpawner;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@Mod(modid = "fleggart")
+public class FleggartMod {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        GameRegistry.registerWorldGenerator(new AncientDebrisSpawner(), 0);
+        System.out.println("✅ 世界生成器注册成功！");
+    }
+}

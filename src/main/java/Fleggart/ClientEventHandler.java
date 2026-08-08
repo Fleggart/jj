c4a23873da7e39ffc36117a0329c9124b104ef25
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-08 15:47:20 +0800

    Update ClientEventHandler.java

--- src/main/java/Fleggart/ClientEventHandler.java
+++ src/main/java/Fleggart/ClientEventHandler.java
@@ -1,26 +1,26 @@
-package Fleggart;
+package Fleggart.client;
 
+import Fleggart.FleggartMod;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
+@Mod.EventBusSubscriber(modid = "fleggart", value = Side.CLIENT)
 public class ClientEventHandler {
 
     @SubscribeEvent
-    public void registerModels(ModelRegistryEvent event) {
-
+    public static void registerModels(ModelRegistryEvent event) {  
         ModelLoader.setCustomModelResourceLocation(
                 Item.getItemFromBlock(FleggartMod.ANCIENT_DEBRIS_BLOCK),
                 0,
                 new ModelResourceLocation(
-                        "fleggart:ancient_debris_block",
+                        FleggartMod.ANCIENT_DEBRIS_BLOCK.getRegistryName(),
                         "inventory"
                 )
         );
     }
-}
+} 
